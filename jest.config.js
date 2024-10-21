04a9b9c7d1074fa4f287871de77a66a3dbc72107
Beatriz <40210313@esmad.ipp.pt>
2024-10-21 20:09:59 +0100

    test

--- jest.config.js
+++ jest.config.js
@@ -12,11 +12,14 @@ const config = {
   coverageProvider: "v8",
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest", // Transform .vue files with vue3-jest
-    "^.+\\.js$": "esbuild-jest", // Transform .js files with esbuild-jest for V8 support
+    "^.+\\.js$": "babel-jest", // Transform .js files with esbuild-jest for V8 support
   },
   testEnvironment: "jsdom", // Set the test environment to jsdom for Vue components
   moduleFileExtensions: ["js", "json", "vue"],
   testMatch: ["**/__tests__/**/*.test.js"],
+  transformIgnorePatterns: [
+    "node_modules/(?!axios)", // Ensure that axios is transformed
+  ],
 };
 
 module.exports = config;
